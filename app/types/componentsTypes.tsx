e903a7f50a8431342d71fa0188d17fd1b27098fd
Mounas82 <abdulrehman03082007@gmail.com>
2024-05-28 18:31:42 +0500

    fixed errors

--- app/types/componentsTypes.tsx
+++ app/types/componentsTypes.tsx
@@ -2,10 +2,9 @@ export type cardType = {
     path:string,
     heading:string,
     text:string,
-    link:string
+    link?:string
 }
 export type barType = {
     heading:string,
     text:string
-    link:string
 }
